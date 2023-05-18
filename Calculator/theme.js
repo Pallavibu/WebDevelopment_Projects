b8686a3c1b01e0e709474ddb4d321f90be1eb8aa
PALLAVI B <119939134+Pallavibu@users.noreply.github.com>
2023-05-18 06:53:02 +0000

    Worked on Weather API

--- Calculator/theme.js
+++ Calculator/theme.js
@@ -1,11 +1,11 @@
-function myFunction(){
-    var element = document.body;
+
+  function myFunction(){
+    var element = document.querySelector('body');
     if (element.classList.contains("dark-mode")){
       element.classList.remove("dark-mode");
       element.classList.add("pink-mode");
     } else if (element.classList.contains("pink-mode")) {
       element.classList.remove("pink-mode");
-      // add any other classes you want to toggle
     } else {
       element.classList.add("dark-mode");
     }
